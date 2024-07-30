aa2f17900f165eed9bdf162b31c67d8ed96b9d84
leydi montero <leidy_940@hotmail.es>
2024-07-30 18:51:05 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'painter-ai-ionic',
+  webDir: 'www'
+};
+
+export default config;
